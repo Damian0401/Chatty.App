fc08c5f0aacb27ce03348f142d13acda1befeba5
Damian0401 <szkola_abc@o2.pl>
2022-09-22 23:30:18 +0200

    Added emoji picker

--- Chatty/client-app/src/features/chat/room/ChatRoom.tsx
+++ Chatty/client-app/src/features/chat/room/ChatRoom.tsx
@@ -1,11 +1,12 @@
-import { Box, Button, Center, Container, Flex, Input } from "@chakra-ui/react";
+import { Button, Center, Container, Flex, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import { observer } from "mobx-react-lite";
-import { RefObject, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useStore } from "../../../app/stores/store";
 import ChatRoomHeader from "./ChatRoomHeader";
 import ChatMessageContainer from "../message/ChatMessageContainer";
 import { toast } from "react-toastify";
+import Picker, { IEmojiData } from 'emoji-picker-react';
 
 
 export default observer(function ChatRoom() {
@@ -14,6 +15,8 @@ export default observer(function ChatRoom() {
     const { id } = useParams<{ id: string }>();
 
     const [messageBody, setMessageBody] = useState('');
+
+    const [isPickerVisible, setIsPickerVisible] = useState(false);
     
     const bottomRef = useRef<HTMLDivElement | null>(null);
 
@@ -35,6 +38,10 @@ export default observer(function ChatRoom() {
         setMessageBody('');
     }
 
+    const handleEmojiClick = (data: IEmojiData) => {
+        setMessageBody(messageBody + data.emoji);
+        setIsPickerVisible(false);
+    }
 
     return (
         <Center>
@@ -47,20 +54,45 @@ export default observer(function ChatRoom() {
             >
                 <ChatRoomHeader />
                 <Flex flexDir='column' height='100%'>
-                    <Flex flexDir='column-reverse' height='100%' width='100%' overflow='auto' p='2' pb='1'>
+                    <Flex
+                        flexDir='column-reverse'
+                        height='100%' width='100%'
+                        overflow='auto' p='2' pb='1'
+                        onClick={() => setIsPickerVisible(false)}
+                    >
                         <div ref={bottomRef} />
                         {chatStore.selectedRoom && chatStore.selectedRoom.messages?.map((message) => (
                             <ChatMessageContainer message={message} key={message.id} />
                         ))}
                     </Flex>
                     <Flex width='100%'>
-                        <Input
-                            borderRadius='0 0 0 1rem'
-                            value={messageBody}
-                            border='none'
-                            bgColor='whiteAlpha.300'
-                            onChange={e => setMessageBody(e.target.value)}
-                        />
+                        <InputGroup pos='relative'>
+                            <Input
+                                borderRadius='0 0 0 1rem'
+                                value={messageBody}
+                                border='none'
+                                bgColor='whiteAlpha.300'
+                                onChange={e => setMessageBody(e.target.value)}
+                                onClick={() => setIsPickerVisible(false)}
+                            />
+                            <InputRightElement children={
+                                <>
+                                    <Button
+                                        bgColor='transparent'
+                                        _hover={{ bgColor: 'whiteAlpha.200' }}
+                                        onClick={() => setIsPickerVisible(true)}
+                                    >
+                                        🙂
+                                    </Button>
+                                    {isPickerVisible &&
+                                        <Picker
+                                            disableSearchBar
+                                            onEmojiClick={(_, d) => handleEmojiClick(d)}
+                                            pickerStyle={{ position: 'absolute', bottom: '0' }}
+                                        />}
+                                </>
+                            } />
+                        </InputGroup>
                         <Button
                             variant='main-style'
                             bgColor='blue.600'
